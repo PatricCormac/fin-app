e0c1796b80cae66314072a6d586e0429ca31af53
patriccormac <socolov1897@gmail.com>
2021-07-05 00:07:42 +0500

    geting data to home page, cors, dataApi

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 
 const router = require('./routes/index')
 const PORT = process.env.PORT || 5000
 const app = express()
 
+app.use(cors())
 app.use('/api', router);
 
 async function start() {
--- server/constrollers/dataController.js
+++ server/constrollers/dataController.js
@@ -3,12 +3,6 @@ const xlsxFile = require('read-excel-file/node')
 class Data {
   async getData(req, res) {
     const data = await xlsxFile("./data/data.xlsx")
-    data.forEach((col)=>{
-      col.forEach((data)=>{
-        console.log(data);
-        console.log(typeof data);
-      })
-    })
     return res.send(data)
   }
 }
